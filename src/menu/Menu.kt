54b9797c6420fe2c8c5b0654987026e2e7eb9639
Adriano-konig <konigadri87@hotmail.comgit config --global user.email konigadri87@hotmail.com>
2022-05-19 15:29:32 -0300

    Implementar funcao do menu

--- src/menu/Menu.kt
+++ src/menu/Menu.kt
@@ -1,21 +1,17 @@
 package menu
 
 import listacompras.ListaCompras.Companion.informarNomeAlimento
+import listacompras.ListaCompras.Companion.informarQuantidadeLegumeOutros
 import listacompras.ListaCompras.Companion.informarQuantidadeVerduraGrao
 import kotlin.system.exitProcess
 
 class Menu {
     companion object {
-        var listaAlimentos: HashMap<String, Number> = HashMap()
+        private var listaAlimentos: HashMap<String, Number> = HashMap()
+
 
         fun menu() {
-            println("Digite o tipo de alimento que deseja incluir na lista")
-            println("Verdura")
-            println("Legume")
-            println("Grão")
-            println("Outros")
-            println("Ver lista")
-            println("Sair")
+            println("Digite o tipo de alimento que deseja incluir na lista\n" + "Verdura\n" + "Legume\n" + "Grão\n" + "Outros\n" + "ver Lista\n" + "Sair")
             val alimento = readln().lowercase()
 
             if ((alimento != "verdura") && (alimento != "legume") && (alimento != "grão") && (alimento != "grao") && (alimento != "outros") && (alimento != "ver lista") && (alimento != "sair")) {
@@ -32,7 +28,9 @@ class Menu {
                     menu()
                 }
                 "legume", "outros" -> {
-                    //TODO: terminar de implementar lógica (1/3)
+                    val quantidade = informarQuantidadeLegumeOutros(alimento)
+                    val nome = informarNomeAlimento(alimento)
+                    menu()
                 }
                 "ver lista" -> {
                     if (listaAlimentos.isEmpty()) {
@@ -46,11 +44,13 @@ class Menu {
                         }
                     }
                 }
-                    "sair" -> {
+                "sair" -> {
                     println("Até breve!")
                     exitProcess(0)
                 }
             }
         }
+
+
     }
 }
