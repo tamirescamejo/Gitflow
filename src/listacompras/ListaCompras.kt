7fdfb80b86fd264bcf8de3f8f3118d365b645cb5
oliveiranatalia <natalia.oliveira@zup.com.br>
2022-05-19 14:55:50 -0300

    Corrigir bug excecao quantidade

--- src/listacompras/ListaCompras.kt
+++ src/listacompras/ListaCompras.kt
@@ -3,14 +3,13 @@ package listacompras
 class ListaCompras {
     companion object {
         fun informarQuantidadeVerduraGrao(alimento: String):Double{
-            println("Informe a quantidade de $alimento em gramas: ")
-            val input = readln()
-
-            if (input.isEmpty() || input.isBlank()) {
-                throw UnsupportedOperationException("Não é permitido inserir valor vazio")
-            }
             var gramas = 0.0
             try {
+                print("Informe a quantidade de $alimento em gramas: ")
+                val input = readln()
+                if (input.isEmpty() || input.isBlank()) {
+                    throw UnsupportedOperationException("Não é permitido inserir valor vazio")
+                }
                 gramas = input.toDouble()
                 if (gramas < 0.0) {
                     println("Não é permitido números negativos, tente novamente")
@@ -18,28 +17,30 @@ class ListaCompras {
                 }
             } catch (exception: NumberFormatException) {
                 println("Não é permitido texto, somente número")
+                informarQuantidadeVerduraGrao(alimento)
             }
             return gramas
         }
 
         fun informarNomeAlimento(alimento: String): String {
-            println("Informe o tipo $alimento a ser adicionado na lista: ")
+            print("Informe o tipo $alimento a ser adicionado na lista: ")
             val nome = readln()
 
-            if (nome.isEmpty() || nome.isBlank()) {
+            if (nome.isEmpty() || nome.isBlank()){
                 throw UnsupportedOperationException("Não é permitido inserir valor vazio")
+            }else {
+                return nome
             }
-            return nome
         }
-        fun informarQuantidadeLegumeOutros(alimento: String):Int{
-            println("Informe a quantidade de $alimento em unidades: ")
-            val input = readln()
 
-            if (input.isEmpty() || input.isBlank()) {
-                throw UnsupportedOperationException("Não é permitido inserir valor vazio")
-            }
+        fun informarQuantidadeLegumeOutros(alimento: String):Int{
             var unidades = 0
             try {
+                print("Informe a quantidade de $alimento em unidades: ")
+                val input = readln()
+                if (input.isEmpty() || input.isBlank()) {
+                    throw UnsupportedOperationException("Não é permitido inserir valor vazio")
+                }
                 unidades = input.toInt()
                 if (unidades < 0) {
                     println("Não é permitido números negativos, tente novamente")
@@ -47,6 +48,7 @@ class ListaCompras {
                 }
             } catch (exception: NumberFormatException) {
                 println("Não é permitido texto, somente número")
+                informarQuantidadeLegumeOutros(alimento)
             }
             return unidades
         }
